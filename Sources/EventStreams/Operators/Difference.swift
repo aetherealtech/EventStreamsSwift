3cd47ca322defc7100c867dec00b54ea12cd555b
Daniel Coleman <dcoleman0711@gmail.com>
2022-05-21 18:29:12 -0500

    Optional initialValue in differences operator

--- Sources/EventStreams/Operators/Difference.swift
+++ Sources/EventStreams/Operators/Difference.swift
@@ -8,11 +8,13 @@ import Observer
 extension EventStream {
 
     public func difference<Result>(
+        initialValue: Value? = nil,
         _ differentiator: @escaping (Value, Value) -> Result
     ) -> EventStream<Result> {
 
         DifferenceEventStream(
             source: self,
+            initialValue: initialValue,
             differentiator: differentiator
         )
     }
@@ -22,12 +24,13 @@ class DifferenceEventStream<Value, Result> : EventStream<Result>
 {
     init(
         source: EventStream<Value>,
+        initialValue: Value?,
         differentiator: @escaping (Value, Value) -> Result
     ) {
 
         self.source = source
 
-        var lastOpt: Value?
+        var lastOpt = initialValue
 
         let channel = SimpleChannel<Event<Result>>()
 
